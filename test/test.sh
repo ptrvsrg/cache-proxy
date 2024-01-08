dfd463d7da64eddc70b3523b532ae2667e622365
ptrvsrg <s.petrov1@g.nsu.ru>
2024-01-09 04:57:26 +0700

    Добавлен код выхода; Рефакторинг

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+EXIT_CODE=0
+
 echo "1. Test with many requests"
 mkdir -p "test_many_requests"
 TEST_COUNT=100
@@ -7,7 +9,7 @@ for i in $(seq 1 $TEST_COUNT); do \
   wget "http://www.ccfit.nsu.ru/~rzheutskiy/test_files" \
   -e use_proxy=on \
   -e http_proxy="localhost:8080" \
-  -O "./test_many_requests/$i.html" > /dev/null 2>&1 & \
+  -O "./test_many_requests/$i.html" & \
 done
 wait
 echo "1. Checking"
@@ -15,26 +17,36 @@ wget "http://www.ccfit.nsu.ru/~rzheutskiy/test_files" \
   -O "./test_many_requests/correct.html" > /dev/null 2>&1
 SUCCESSFUL_TESTS=0
 for i in $(seq 1 $TEST_COUNT); do \
-  diff "./test_many_requests/correct.html" "./test_many_requests/$i.html" > /dev/null 2>&1 && \
-    echo "1. Test $i.html - SUCCESS" && \
-    ((SUCCESSFUL_TESTS = SUCCESSFUL_TESTS + 1)) || \
+  RESULT=$(diff "./test_many_requests/correct.html" "./test_many_requests/$i.html" > /dev/null 2>&1)
+  if [[ "$RESULT" -eq 0 ]]; then \
+    echo "1. Test $i.html - SUCCESS"
+    ((SUCCESSFUL_TESTS = SUCCESSFUL_TESTS + 1))
+  else
     echo "1. Test $i.html - FAIL"
+    EXIT_CODE=1
+  fi
 done
 echo "1. Successful tests: $SUCCESSFUL_TESTS/$TEST_COUNT"
 
 echo "2. Test with big request"
 mkdir -p "test_big_request"
 wget "http://www.ccfit.nsu.ru/~rzheutskiy/test_files/50mb.dat" \
+  -O "./test_big_request/correct.dat" > /dev/null 2>&1 &
+wget "http://www.ccfit.nsu.ru/~rzheutskiy/test_files/50mb.dat" \
   -e use_proxy=on \
   -e http_proxy="localhost:8080" \
-  -O "./test_big_request/50mb.dat" > /dev/null 2>&1 &
-wget "http://www.ccfit.nsu.ru/~rzheutskiy/test_files/50mb.dat" \
-  -O "./test_big_request/correct.dat" > /dev/null 2>&1 &
+  -O "./test_big_request/50mb.dat"
 wait
 echo "2. Checking"
 SUCCESSFUL_TESTS=0
-diff "./test_big_request/correct.dat" "./test_big_request/50mb.dat" > /dev/null 2>&1 && \
-  echo "2. Test 50mb.dat - SUCCESS" && \
-  ((SUCCESSFUL_TESTS = SUCCESSFUL_TESTS + 1)) || \
-  echo "2. Test 50mb.dat - FAIL"
+RESULT=$(diff "./test_big_request/correct.dat" "./test_big_request/50mb.dat" > /dev/null 2>&1)
+  if [[ "$RESULT" -eq 0 ]]; then \
+    echo "2. Test 50mb.dat - SUCCESS"
+    ((SUCCESSFUL_TESTS = SUCCESSFUL_TESTS + 1))
+  else
+    echo "2. Test 50mb.dat - FAIL"
+    EXIT_CODE=1
+  fi
 echo "2. Successful tests: $SUCCESSFUL_TESTS/1"
+
+exit $EXIT_CODE
