bc3fac2c047d92096b684a889ff6a1a941896888
ptrvsrg <s.petrov1@g.nsu.ru>
2024-01-09 05:57:38 +0700

    Рефакторинг

--- test/test.sh
+++ test/test.sh
@@ -4,17 +4,18 @@ EXIT_CODE=0
 
 echo "1. Test with many requests"
 mkdir -p "test_many_requests"
+echo "1. Sending..."
 TEST_COUNT=100
 for i in $(seq 1 $TEST_COUNT); do \
   wget "http://www.ccfit.nsu.ru/~rzheutskiy/test_files" \
   -e use_proxy=on \
   -e http_proxy="localhost:8080" \
-  -O "./test_many_requests/$i.html" & \
+  -O "./test_many_requests/$i.html" > /dev/null 2>&1 & \
 done
-wait
-echo "1. Checking"
 wget "http://www.ccfit.nsu.ru/~rzheutskiy/test_files" \
-  -O "./test_many_requests/correct.html" > /dev/null 2>&1
+  -O "./test_many_requests/correct.html" > /dev/null 2>&1 &
+wait
+echo "1. Checking..."
 SUCCESSFUL_TESTS=0
 for i in $(seq 1 $TEST_COUNT); do \
   RESULT=$(diff "./test_many_requests/correct.html" "./test_many_requests/$i.html" > /dev/null 2>&1)
@@ -28,8 +29,10 @@ for i in $(seq 1 $TEST_COUNT); do \
 done
 echo "1. Successful tests: $SUCCESSFUL_TESTS/$TEST_COUNT"
 
+
 echo "2. Test with big request"
 mkdir -p "test_big_request"
+echo "2. Sending..."
 wget "http://www.ccfit.nsu.ru/~rzheutskiy/test_files/50mb.dat" \
   -O "./test_big_request/correct.dat" > /dev/null 2>&1 &
 wget "http://www.ccfit.nsu.ru/~rzheutskiy/test_files/50mb.dat" \
@@ -37,7 +40,7 @@ wget "http://www.ccfit.nsu.ru/~rzheutskiy/test_files/50mb.dat" \
   -e http_proxy="localhost:8080" \
   -O "./test_big_request/50mb.dat"
 wait
-echo "2. Checking"
+echo "2. Checking..."
 SUCCESSFUL_TESTS=0
 RESULT=$(diff "./test_big_request/correct.dat" "./test_big_request/50mb.dat" > /dev/null 2>&1)
   if [[ "$RESULT" -eq 0 ]]; then \
